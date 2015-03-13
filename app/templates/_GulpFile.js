d55b2f24c4108f36eb151a404b105b1aa440b76e
jrah <jjames.home@gmail.com>
2015-03-13 20:41:53 +0000

    added gulp-header to add wp media data after css is compressed

--- app/templates/_GulpFile.js
+++ app/templates/_GulpFile.js
@@ -21,6 +21,21 @@ var projectName = "<%= themename %>";
 var themeName = '_' + projectName;
 var themeDir = 'themes/' + themeName + '/';
 
+// ### WP Metadata
+var banner = [
+'/*',
+' * Theme Name: <%= themename %>',
+' * Theme URI: <%= themeuri %>',
+' * Author: <%= author %>',
+' * Author URI: <%= authoruri %>',
+' * Description: <%= themedescription %>',
+' * Version: 1.0',
+' * License: GNU General Public License v2 or later',
+' * License URI: http://www.gnu.org/licenses/gpl-2.0.html',
+' * Tags: ',
+' * GPL license',
+' */',
+''].join('\n');
 
 // ## CSS theme location
 var cssDir  = themeDir;
@@ -48,6 +63,20 @@ gulp.task('sass', function() {
    .pipe(reload({stream:true}));
 });
 
+gulp.task('build', function() {
+  // locates Sass
+  return gulp.src(scssDir + '*.scss')
+  // source maps none is true
+  .pipe(sass({'sourcemap=none': true, style: 'compressed'}))
+  // .pipe(minifyCSS({ keepSpecialComments: 1 }))
+    // outputs CSS but before inject banner
+    .pipe(header(banner))
+   .pipe(gulp.dest(cssDir))
+
+   .pipe(notify("Sass has compiled!"))
+   // reloads the browser
+   .pipe(reload({stream:true}));
+});
 
 // compile sass, watch directory for browser sync
 gulp.task('default', ['sass','browser-sync'], function () {
